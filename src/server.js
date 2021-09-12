3564ce37f714e0b9a92261c00c5b50db3b0fd5ff
공민수 <mansukong@gmail.com>
2021-09-13 07:32:15 +0900

    1.4

--- src/server.js
+++ src/server.js
@@ -1,16 +1,29 @@
+import http from "http";
+import WebSocket from "ws";
 import express from "express";
-import path from "path";
-const __dirname = path.resolve();
 
 const app = express();
-const port = 3000;
 
 app.set("view engine", "pug");
-app.set("views", __dirname + "/src/views");
-app.use("/public", express.static(__dirname + "/src/public"));
+app.set("views", __dirname + "/views");
+app.use("/public", express.static(__dirname + "/public"));
+app.get("/", (_, res) => res.render("home"));
+app.get("/*", (_, res) => res.redirect("/"));
 
-app.get("/", (req, res) => res.render("home"));
+const handleListen = () => console.log(`Listening on http://localhost:3000`);
 
-const handleListen = () => console.log(`Litening on http://localhost:${port}`);
+const server = http.createServer(app);
+const wss = new WebSocket.Server({ server });
 
-app.listen(port, handleListen);
+wss.on("connection", (socket) => {
+  console.log("Connected to Browser ⚡");
+  socket.on("close", () => {
+    console.log("Disconnected from Browser ❌");
+  });
+  socket.on("message", (message) => {
+    console.log(message.toString("utf8"));
+  });
+  socket.send("hello");
+});
+
+server.listen(3000, handleListen);
